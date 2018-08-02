dd51e35fa10d5e2c84bc85382927d762b21a2e26
AviramAshkenazi <31100069+AviramAshkenazi@users.noreply.github.com>
2018-08-02 12:50:24 +0300

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+    specs: [__dirname + '/test.js'],
+    onPrepare: function() {
+      browser.driver.manage().window().maximize();
+      global.EC = protractor.ExpectedConditions;
+    },
+    framework: 'jasmine2',
+    jasmineNodeOpts: {
+      showColors: true
+    },
+  };
